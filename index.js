392030ebdf350dec7c926c89f7d9098c7df73607
lebv28@gmail.com <lebv28@gmail.com>
2021-08-28 18:32:49 +0700

    fist commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Khai báo và. Gọi module: express, mssql, body-parser, ejs
+var express = require('express');
+var mssql = require('mssql');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 5500;
+
+app.listen(port, function() {
+    console.log(`Sever runing port: http://localhost:${port}`)
+});
+app.use(express.static('Public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+// Kết thúc khai báo:
